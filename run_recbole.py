9f2eee762bc9758117e1d42118b92d4e715e5b21
Nikhil Agarwal <nikhil.ag.0209@gmail.com>
2022-01-12 22:17:29 -0500

    changes to run sasrec on steam dataset

--- run_recbole.py
+++ run_recbole.py
@@ -20,6 +20,8 @@ if __name__ == '__main__':
     parser.add_argument('--config_files', type=str, default=None, help='config files')
 
     args, _ = parser.parse_known_args()
-
+    parameter_dict = {
+       'neg_sampling': None,
+    }
     config_file_list = args.config_files.strip().split(' ') if args.config_files else None
-    run_recbole(model=args.model, dataset=args.dataset, config_file_list=config_file_list)
+    run_recbole(model=args.model, dataset=args.dataset, config_file_list=config_file_list, config_dict=parameter_dict)
